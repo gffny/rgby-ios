1bd94cd5550d19c5dff4526212883603728000e6
gffny <john@gffny.com>
2019-01-05 12:03:53 -0500

    Added context data to incident table and updated formating of text

--- rgby-ios/view/incident/RGBYIncidentTableCell.swift
+++ rgby-ios/view/incident/RGBYIncidentTableCell.swift
@@ -13,13 +13,25 @@ class RGBYIncidentTableCell: UITableViewCell {
     static var nibName = "RGBYIncidentTableCell"
 
     @IBOutlet weak var teamLabel: UILabel!
+    @IBOutlet weak var teamSubLabel: UILabel!
     @IBOutlet weak var incidentIcon: UIImageView!
     @IBOutlet weak var oppositionLabel: UILabel!
-
+    @IBOutlet weak var oppositionSubLabel: UILabel!
+    
     var matchEvent: RGBYMatchEvent? {
         didSet {
             self.incidentIcon.image = matchEvent?.eventType?.eventIcon ?? UIImage(named: "AppIcon")
-            self.teamLabel.text = matchEvent!.isMyTeam! ? RGBYUtils.formatPlayerName(player: matchEvent!.subject) : matchEvent!.subjectPosition?.displayName
+            if matchEvent!.isMyTeam! {
+                self.oppositionLabel.text = ""
+                self.oppositionSubLabel.text = ""
+                self.teamLabel.text = RGBYUtils.formatPlayerName(player: matchEvent!.subject)
+                self.teamSubLabel.text = "\(matchEvent!.eventTime) \(matchEvent!.eventType?.displayName ?? "")"
+            } else {
+                self.teamLabel.text = ""
+                self.teamSubLabel.text = ""
+                self.oppositionLabel.text = matchEvent!.subjectPosition?.displayName
+                self.oppositionSubLabel.text = "\(matchEvent!.eventTime) \(matchEvent!.eventType?.displayName ?? "")"
+            }
         }
     }
 
